22a4858bf0db042974e87ed963e43d7aaf37358a
mmcgarry13 <mcgarry31@outlook.com>
2024-10-16 15:57:23 -0400

    Added methods to historyservice for reading/writing searchHistory json file

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,11 +12,13 @@ const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
 
+
+
+// TODO: Implement middleware for parsing JSON and urlencoded form data
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('../client'));
-
-// TODO: Implement middleware for parsing JSON and urlencoded form data
 
 // TODO: Implement middleware to connect the routes
 app.use(routes);
--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,15 +1,62 @@
+import fs from 'node:fs/promises';
+import { v4 as uuidv4 } from 'uuid';
+
 // TODO: Define a City class with name and id properties
+class City {
+  name: string
+  id: number // maybe?
+
+  constructor (
+    name: string,
+    id: number,
+
+  ) {
+    this.name = name;
+    this.id = id;
+  }};
 
 // TODO: Complete the HistoryService class
 class HistoryService {
   // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
+  private async read() {
+    return await fs.readFile('searchHistory.json', {
+      flag: 'a+',
+      encoding: 'utf8',
+    })};
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  // private async write(cities: City[]) {}
+
+    private async write(cities: City[]) {
+      return await fs.writeFile(
+        'db/searchHistory.json',
+        JSON.stringify(cities, null, '\t')
+      );
+    };
+
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  // async getCities() {}
+  async getCities() {
+    return await this.read().then((cities) => {
+      let parsedFeedback: City[];
+
+      // If feedback isn't an array or can't be turned into one, send back a new empty array
+      try {
+        parsedFeedback = [].concat(JSON.parse(cities));
+      } catch (err) {
+        parsedFeedback = [];
+      }
+
+      return parsedFeedback;
+    });
+  }
+
+
   // TODO Define an addCity method that adds a city to the searchHistory.json file
-  // async addCity(city: string) {}
+  async addCity(city: string) {
+    return await fs.appendFile(
+      'db/searchHistory.json',
+        JSON.stringify(city, null, '\t')
+    )
+
+  };
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
   // async removeCity(id: string) {}
 }
